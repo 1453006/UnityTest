1c2a81399c27b33a6db3c36436e12c4bc7455d65
1453006 <1453006@student.hcmus.edu.vn>
2017-11-03 14:26:19 +0700

    commit

---
+++ UnityProject/Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move_x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float move_z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(move_x, 0, move_z);
+	}
+}
